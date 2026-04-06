ee9c4f115c193afeb6e294cd931d812e6e7e8c2c
mrgokul77 <gokulpoudel558>
2026-04-06 12:01:20 +0545

    fix: use EMAIL_HOST_USER as from_email in util.send_email

--- backend/accounts/utils.py
+++ backend/accounts/utils.py
@@ -1,14 +1,13 @@
 from django.core.mail import EmailMessage
-import os
+from django.conf import settings
 
 class util:
-    # helper to send emails - used for OTP and password resets
     @staticmethod
     def send_email(data):
         email = EmailMessage(
             subject=data['subject'],
             body=data['body'],
-            from_email=os.environ.get('EMAIL_FROM'),
+            from_email=settings.EMAIL_HOST_USER,  
             to=[data['to_email']],
         )
         email.send()
